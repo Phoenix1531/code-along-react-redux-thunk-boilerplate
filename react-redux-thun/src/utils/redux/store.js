de0d3a5e02783a599460bd8d4da59c405cde1b1c
phoenix1531 <arjun15310@gmail.com>
2024-01-02 18:15:13 +0530

    done

---
+++ react-redux-thun/src/utils/redux/store.js
@@ -0,0 +1,7 @@
+import {applyMiddleware, legacy_createStore as createStore} from "redux"
+import { reducer } from "./reducer"
+import logger from "redux-logger"
+import {thunk} from "redux-thunk"
+
+export const fetchStore=createStore(reducer,applyMiddleware(logger,thunk)) 
+
